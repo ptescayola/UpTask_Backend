d3fd9389f41ea5f08373c6489eab0742e5a865a8
Pere Torres <ptescayola@gmail.com>
2024-11-13 11:53:31 +0100

    Cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,9 +4,9 @@ export const corsConfig: CorsOptions = {
   origin: function(origin, callback) {
     const whitelist = [process.env.FRONTEND_URL]
 
-    if (process.argv[2] === '--api') {
+    // if (process.argv[2] === '--api') {
       whitelist.push(undefined)
-    }
+    // }
 
     console.log('whitelist', whitelist)
     console.log('origin', origin)
