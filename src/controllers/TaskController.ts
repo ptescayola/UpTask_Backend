fd7fa71e65d428ebb84dad73ddd852b643827bfd
Pere Torres <ptescayola@gmail.com>
2024-12-03 17:17:59 +0100

    Add status on task

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -4,8 +4,11 @@ import Task from '../models/Task'
 export class TaskController {
   static createTask = async (req: Request, res: Response) => {
     try {
-      const task = new Task(req.body)     
-      task.project = req.project.id
+      const task = new Task({
+        ...req.body,
+        project: req.project.id,
+        status: req.body.status || 'pending'
+      }) 
       req.project.tasks.push(task.id)
       await Promise.allSettled([task.save(), req.project.save() ])
 
