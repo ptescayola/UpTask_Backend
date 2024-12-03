1e6aaaa7e4541a9c8a3a874a44f43138598c2c7f
Pere Torres <ptescayola@gmail.com>
2024-12-03 17:28:18 +0100

    AllowedOrigins

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,13 +2,13 @@ import { CorsOptions } from 'cors'
 
 export const corsConfig: CorsOptions = {
   origin: function(origin, callback) {
-    const whitelist = [process.env.FRONTEND_URL]
+    const allowedOrigins = [process.env.FRONTEND_URL]
 
     if (process.argv[2] === '--api') {
-      whitelist.push(undefined)
+      allowedOrigins.push(undefined)
     }
 
-    if (whitelist.includes(origin)) {
+    if (allowedOrigins.includes(origin)) {
       callback(null, true)
     } else {
       callback(new Error('CORS Error'))
