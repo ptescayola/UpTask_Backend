b4b731ffa20d7e31c535eeb750e0c4ba559da4d9
Pere Torres <ptescayola@gmail.com>
2024-12-03 17:05:49 +0100

    Minor fix

--- src/models/Note.ts
+++ src/models/Note.ts
@@ -1,9 +1,9 @@
 import mongoose, { Schema, Document, Types } from 'mongoose'
 
 export interface INote extends Document {
-    content: string
-    createdBy: Types.ObjectId
-    task: Types.ObjectId
+  content: string
+  createdBy: Types.ObjectId
+  task: Types.ObjectId
 }
 
 const NoteSchema: Schema = new Schema({
